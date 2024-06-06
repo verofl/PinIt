b87ce74204d5794a617a80adb0a4b3055c2d9894
Veronica <veronicaflatto@gmail.com>
2024-06-06 16:07:21 -0700

    added demo-user login

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -53,6 +53,13 @@ function LoginFormModal() {
         </label>
         {errors.password && <p>{errors.password}</p>}
         <button type="submit">Log In</button>
+        <button
+          onClick={() => {
+            setEmail("demo@aa.io"), setPassword("password");
+          }}
+        >
+          Demo User
+        </button>
       </form>
     </>
   );
