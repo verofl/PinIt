bd02e7c677ad562b4ad539541d4975dbb09c34c8
Veronica <veronicaflatto@gmail.com>
2024-07-02 18:20:47 -0700

    created user profile page

--- react-vite/src/components/ProfilePage/ProfilePage.jsx
+++ react-vite/src/components/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,29 @@
 import "./ProfilePage.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUserPinsThunk } from "../../redux/pin";
+import CreatedPins from "./CreatedPins";
 
 export const ProfilePage = () => {
-  return <h1 className="profile-page-header">This will be the Profile Page</h1>;
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.session.user);
+  const user_pins = useSelector((state) => state.pinReducer);
+  let pinsArray = Object.values(user_pins);
+
+  console.log("USER INFO ==>", user);
+  console.log("USER PINS INFO ==>", user_pins);
+  console.log("PINS ARRAY ===>", pinsArray);
+
+  useEffect(() => {
+    dispatch(loadUserPinsThunk(user.id));
+  }, [dispatch]);
+  return (
+    <div className="profile-page-div">
+      <img src={user.profile_picture} className="pfp-profile-page"></img>
+      <h1 className="profile-page-header">Created Pins</h1>
+      <div className="user-pins-cont">
+        <CreatedPins />
+      </div>
+    </div>
+  );
 };
