de9103c73ed2b9663e2f7e1bdf9f58e55f484afe
Veronica <veronicaflatto@gmail.com>
2024-07-12 15:41:58 -0700

    created about section and added my image

--- react-vite/src/components/About/About.jsx
+++ react-vite/src/components/About/About.jsx
@@ -5,13 +5,17 @@ export const About = () => {
   return (
     <div className="about-page">
       <h1>About Me</h1>
-      <Link to="" target="_blank">
+      <img
+        src="https://res.cloudinary.com/djqcfdrbm/image/upload/v1720823942/image_ts9ftk.png"
+        className="my-image"
+      />
+      <Link to="https://www.linkedin.com/in/veronica-flatto/" target="_blank">
         Linkedin
       </Link>
-      <Link to="" target="_blank">
+      <Link to="https://github.com/verofl" target="_blank">
         Github
       </Link>
-      <Link to="" target="_blank">
+      <Link to="https://verofl.github.io/" target="_blank">
         Portfolio
       </Link>
     </div>
