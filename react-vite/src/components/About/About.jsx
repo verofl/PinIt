2152a4205033b621335b1a3853802e05d55cff35
Veronica <veronicaflatto@gmail.com>
2024-07-12 16:33:16 -0700

    styled about me page

--- react-vite/src/components/About/About.jsx
+++ react-vite/src/components/About/About.jsx
@@ -4,20 +4,37 @@ import { Link } from "react-router-dom";
 export const About = () => {
   return (
     <div className="about-page">
-      <h1>About Me</h1>
+      <h1 className="about-header">About Me</h1>
       <img
         src="https://res.cloudinary.com/djqcfdrbm/image/upload/v1720823942/image_ts9ftk.png"
         className="my-image"
       />
-      <Link to="https://www.linkedin.com/in/veronica-flatto/" target="_blank">
-        Linkedin
-      </Link>
-      <Link to="https://github.com/verofl" target="_blank">
-        Github
-      </Link>
-      <Link to="https://verofl.github.io/" target="_blank">
-        Portfolio
-      </Link>
+      <div className="about-links">
+        <Link
+          to="https://www.linkedin.com/in/veronica-flatto/"
+          target="_blank"
+          className="links"
+        >
+          Linkedin
+        </Link>
+        <Link to="https://github.com/verofl" target="_blank" className="links">
+          Github
+        </Link>
+        <Link to="https://verofl.github.io/" target="_blank" className="links">
+          Portfolio
+        </Link>
+      </div>
+      {/* <p>Hi! My name is Veronica Flatto.</p> */}
+      {/* </br>  */}
+      <p className="about-me-p">
+        {" "}
+        Hi! My name is Veronica Flatto. I am a software engineer with a
+        background in full-stack development, holding a BA in Psychology from UC
+        Irvine and a Full Stack Web Development certification from App Academy.
+        My expertise includes JavaScript, Python, Flask, React, Redux, SQL, and
+        more. This is my fullstack project that replicates the numerous features
+        that Pinterest has. I hope you enjoy it!
+      </p>
     </div>
   );
 };
